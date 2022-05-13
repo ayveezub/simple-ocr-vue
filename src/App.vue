c8f36dd754d3dc934ee9523fd9e2e93adb4de5e4
ayveezub <ayveezub@gmail.com>
2022-05-13 23:50:08 +0300

    add Home view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
+<template lang="pug">
+main
+  Home
+</template>
+
 <script setup>
-import { createWorker } from 'tesseract.js'
-const worker = createWorker()
+import Home from '@/views/Home.vue'
 </script>
 
-<template></template>
+<style>
+@font-face
+  font-family: "Iosevka"
+  src: url("@/assets/fonts/iosevka-light.woff2") format("woff2")
 
-<style></style>
+body
+  background-color: #eee8d5
+
+#app
+  font-family: Iosevka, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  font-size: large
+  text-align: left
+  color: #657b83
+
+main
+  display: grid
+  grid-template-columns: 1fr min(100%, 65ch) 1fr
+
+main > *
+  grid-column: 2
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,73 @@
+<template lang="pug">
+.home
+  label.select-image
+    | Click To Select Image
+    input(
+      type='file'
+      name='select-image__input'
+      accept='.jpg, .jpeg, .png'
+      style={opacity: 0}
+    )
+  label.select-language
+    | lang:
+    select
+      option(value='en+ru') en+ru
+  label.recognize-image-button
+    | Recognize
+  textarea.recognized-text(readonly='true', rows='18')
+</template>
+
+<script setup>
+import { createWorker } from 'tesseract.js'
+const worker = createWorker()
+</script>
+
+<style>
+.home
+  display: grid
+  grid-template-areas:
+    'select-image select-image select-image'
+    'select-language select-language recognize-image-button'
+    'recognized-text recognized-text recognized-text'
+  border-radius: 10px
+  background-color: #eee8d5
+
+label
+  display: flex
+  align-items: center
+  justify-content: center
+  height: 2em
+  margin: 0.5em
+  padding: 0.25em
+  border-style: solid
+  border-width: thin
+  border-radius: 5px
+
+select
+  margin: 0.5em
+
+.select-image
+  flex-direction: column
+  grid-area: select-image
+  height: 9em
+
+.select-language
+  grid-area: select-language
+  border-style: none
+
+.recognize-image-button
+  grid-area: recognize-image-button
+
+.recognized-text
+  font-family: inherit
+  font-size: inherit
+  grid-area: recognized-text
+  margin: 0.5em
+  padding: 0.25em
+  border-style: solid
+  border-width: thin
+  border-radius: 10px
+  background-color: #fdf6e3
+  resize: none
+  overflow-y: scroll
+</style>
